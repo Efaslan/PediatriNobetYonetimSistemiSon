661c3f7aa97ee24ab97d797ec51eeb4c3ecda36c
Emir Aslan <emir.aslan@ogr.sakarya.edu.tr>
2024-12-19 21:30:56 +0300

    migration

--- Migrations/20241219182821_SonMigration.Designer.cs
+++ Migrations/20241219182821_SonMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PediatriNobetYonetimSistemi.Data;
 
@@ -11,9 +12,11 @@ using PediatriNobetYonetimSistemi.Data;
 namespace PediatriNobetYonetimSistemi.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20241219182821_SonMigration")]
+    partial class SonMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20241219182821_SonMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PediatriNobetYonetimSistemi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SonMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
